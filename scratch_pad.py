6456d1eea31ea6f8e10ad6730913ab2f96f53600
dhruv.raj <dhruv.gupta@the-chainless.com>
2024-07-16 19:36:03 +0200

    ask llm

--- scratch_pad.py
+++ scratch_pad.py
@@ -298,4 +298,20 @@ def paragraph_chunking_main():
     for i, chunk in enumerate(chunks):
         print(f"Chunk {i+1}:\n{chunk}\n")
 
-paragraph_chunking_main()
+
+def ask_llm():
+    from ollama import chat
+
+
+    messages = [
+    {
+        'role': 'user',
+        'content': 'Why is the sky blue?',
+    },
+    ]
+
+    for part in chat('mistral', messages=messages, stream=True):
+        print(part['message']['content'], end='', flush=True)
+
+    # end with a newline
+    print()
