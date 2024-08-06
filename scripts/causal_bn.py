99fd13a554b1e5f3d4cd0dfdd02673e1105f7cc3
Dhruv Raj <dhruv.learner@gmail.com>
2024-08-07 00:54:21 +0200

    CausalInference Game1-5

--- scripts/causal_bn.py
+++ scripts/causal_bn.py
@@ -28,4 +28,124 @@ plt.show()
 
 game1 = convert_pgm_to_pgmpy(pgm)
 inference1 = CausalInference(game1)
-print(f"Are there are active backdoor paths? {inference1.is_valid_backdoor_adjustment_set('X', 'Y')}")
+print(
+    f"Are there are active backdoor paths? {not inference1.is_valid_backdoor_adjustment_set('X', 'Y')}"
+)
+adj_sets = inference1.get_all_backdoor_adjustment_sets("X", "Y")
+print(f"If so, what's the possible backdoor adjustment sets? {adj_sets}")
+
+
+# Game 2
+pgm = PGM(shape=[4, 4])
+
+pgm.add_node(daft.Node("X", r"X", 1, 1))
+pgm.add_node(daft.Node("Y", r"Y", 3, 1))
+pgm.add_node(daft.Node("A", r"A", 1, 3))
+pgm.add_node(daft.Node("B", r"B", 2, 3))
+pgm.add_node(daft.Node("C", r"C", 3, 3))
+pgm.add_node(daft.Node("D", r"D", 2, 2))
+pgm.add_node(daft.Node("E", r"E", 2, 1))
+
+
+pgm.add_edge("X", "E")
+pgm.add_edge("A", "X")
+pgm.add_edge("A", "B")
+pgm.add_edge("B", "C")
+pgm.add_edge("D", "B")
+pgm.add_edge("D", "E")
+pgm.add_edge("E", "Y")
+
+pgm.render()
+plt.show()
+
+graph = convert_pgm_to_pgmpy(pgm)
+inference = CausalInference(graph)
+print(
+    f"Are there are active backdoor paths? {not inference.is_valid_backdoor_adjustment_set('X', 'Y')}"
+)
+adj_sets = inference.get_all_backdoor_adjustment_sets("X", "Y")
+print(f"If so, what's the possible backdoor adjustment sets? {adj_sets}")
+
+
+# Game 3
+pgm = PGM(shape=[4, 4])
+
+pgm.add_node(daft.Node("X", r"X", 1, 1))
+pgm.add_node(daft.Node("Y", r"Y", 3, 1))
+pgm.add_node(daft.Node("A", r"A", 2, 1.75))
+pgm.add_node(daft.Node("B", r"B", 2, 3))
+
+
+pgm.add_edge("X", "Y")
+pgm.add_edge("X", "A")
+pgm.add_edge("B", "A")
+pgm.add_edge("B", "X")
+pgm.add_edge("B", "Y")
+
+pgm.render()
+plt.show()
+
+graph = convert_pgm_to_pgmpy(pgm)
+inference = CausalInference(graph)
+print(
+    f"Are there are active backdoor paths? {not inference.is_valid_backdoor_adjustment_set('X', 'Y')}"
+)
+adj_sets = inference.get_all_backdoor_adjustment_sets("X", "Y")
+print(f"If so, what's the possible backdoor adjustment sets? {adj_sets}")
+
+
+
+# Game 4
+pgm = PGM(shape=[4, 4])
+
+pgm.add_node(daft.Node("X", r"X", 1, 1))
+pgm.add_node(daft.Node("Y", r"Y", 3, 1))
+pgm.add_node(daft.Node("A", r"A", 1, 3))
+pgm.add_node(daft.Node("B", r"B", 2, 2))
+pgm.add_node(daft.Node("C", r"C", 3, 3))
+
+
+pgm.add_edge("A", "X")
+pgm.add_edge("A", "B")
+pgm.add_edge("C", "B")
+pgm.add_edge("C", "Y")
+
+pgm.render()
+plt.show()
+
+graph = convert_pgm_to_pgmpy(pgm)
+inference = CausalInference(graph)
+print(
+    f"Are there are active backdoor paths? {not inference.is_valid_backdoor_adjustment_set('X', 'Y')}"
+)
+adj_sets = inference.get_all_backdoor_adjustment_sets("X", "Y")
+print(f"If so, what's the possible backdoor adjustment sets? {adj_sets}")
+
+
+# Game 5
+pgm = PGM(shape=[4, 4])
+
+pgm.add_node(daft.Node("X", r"X", 1, 1))
+pgm.add_node(daft.Node("Y", r"Y", 3, 1))
+pgm.add_node(daft.Node("A", r"A", 1, 3))
+pgm.add_node(daft.Node("B", r"B", 2, 2))
+pgm.add_node(daft.Node("C", r"C", 3, 3))
+
+
+pgm.add_edge("A", "X")
+pgm.add_edge("A", "B")
+pgm.add_edge("C", "B")
+pgm.add_edge("C", "Y")
+pgm.add_edge("X", "Y")
+pgm.add_edge("B", "X")
+
+pgm.render()
+plt.show()
+
+graph = convert_pgm_to_pgmpy(pgm)
+inference = CausalInference(graph)
+print(
+    f"Are there are active backdoor paths? {not inference.is_valid_backdoor_adjustment_set('X', 'Y')}"
+)
+adj_sets = inference.get_all_backdoor_adjustment_sets("X", "Y")
+print(f"If so, what's the possible backdoor adjustment sets? {adj_sets}")
