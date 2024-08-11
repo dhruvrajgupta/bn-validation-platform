557c3182ae95974220e46b615f625b5f1552d4f4
Dhruv Raj <dhruv.learner@gmail.com>
2024-08-11 19:56:10 +0200

    Init for Second best model via SL NOTEARS

---
+++ scripts/second_best_model.py
@@ -0,0 +1,8 @@
+# Here we will use the dataset to perform Structure Learning
+# so that it can be compared to the best model aka (LC/Mstage xdsl file)
+# best model will be stored in the 'best_model' folder
+
+# We will perform NOTEARS Structure Learning as it is the 
+# current SOTA Structure Learning method
+# CausalNex library already contains the method
+
