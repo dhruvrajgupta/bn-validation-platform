8c7b5ec6ab8dc8b5b241bb66796cb70648b81199
dhruv.raj <dhruv.gupta@the-chainless.com>
2024-08-12 22:33:34 +0200

    Second Best model and pyviz graph

--- scripts/second_best_model.py
+++ scripts/second_best_model.py
@@ -6,3 +6,31 @@
 # current SOTA Structure Learning method
 # CausalNex library already contains the method
 # https://proceedings.neurips.cc/paper_files/paper/2018/file/e347c51419ffb23ca3fd5050202f9c3d-Paper.pdf
+
+
+from causalnex.structure import StructureModel
+import warnings
+from pgmpy.readwrite import BIFReader
+
+warnings.filterwarnings("ignore")  # silence warnings
+
+sm = StructureModel()
+
+# Learning structure from the best model to get the second best model
+
+reader = BIFReader("/home/dhruv/Desktop/bn-validation-platform/scripts/best_model/best_model_M_stage.bif")
+model = reader.get_model()
+print(model)
+
+# Create Network to be read by CausalNex
+for edge in model.edges:
+    sm.add_edge(*edge)
+
+from causalnex.plots import plot_structure, NODE_STYLE, EDGE_STYLE
+
+viz = plot_structure(
+    sm,
+    all_node_attributes=NODE_STYLE.WEAK,
+    all_edge_attributes=EDGE_STYLE.WEAK,
+)
+viz.show("M_State_BN.html")
