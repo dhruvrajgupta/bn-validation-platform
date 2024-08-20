44fca448ccfe3091cd0333c6db0c6014176a7543
dhruv.raj <dhruv.gupta@the-chainless.com>
2024-08-20 16:28:13 +0200

    Chunking pdf file

--- scripts/unstructured.py
+++ scripts/unstructured.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 
 import unstructured_client
 from unstructured_client.models import operations, shared
@@ -9,26 +9,58 @@ client = unstructured_client.UnstructuredClient(
     server_url="https://api.unstructured.io/general/v0/general",
 )
 
-print(os.getenv("UNSTRUCTURED_API_KEY"))
+# print(os.getenv("UNSTRUCTURED_API_KEY"))
 
-filename = "/home/dhruv/Desktop/bn-validation-platform/scripts/example-docs/layout-parser-paper-fast.pdf"
-with open(filename, "rb") as f:
-    data = f.read()
+# filename = "/home/dhruv/Desktop/bn-validation-platform/scripts/example-docs/layout-parser-paper-fast.pdf"
+# with open(filename, "rb") as f:
+#     data = f.read()
+
+# req = operations.PartitionRequest(
+#     partition_parameters=shared.PartitionParameters(
+#         files=shared.Files(
+#             content=data,
+#             file_name=filename,
+#         ),
+#         # --- Other partition parameters ---
+#         strategy=shared.Strategy.AUTO,
+#         languages=['eng'],
+#     ),
+# )
+
+# try:
+#     res = client.general.partition(request=req)
+#     print(res.elements[0])
+# except Exception as e:
+#     print(e)
+
+input_filepath = "/home/dhruv/Desktop/bn-validation-platform/scripts/example-docs/layout-parser-paper-fast.pdf"
+output_filepath = "/home/dhruv/Desktop/bn-validation-platform/scripts/example-docs/layout-parser-paper-fast.json"
+
+with open(input_filepath, "rb") as f:
+    files = shared.Files(
+        content=f.read(),
+        file_name=input_filepath
+    )
 
 req = operations.PartitionRequest(
-    partition_parameters=shared.PartitionParameters(
-        files=shared.Files(
-            content=data,
-            file_name=filename,
-        ),
-        # --- Other partition parameters ---
-        strategy=shared.Strategy.AUTO,
-        languages=['eng'],
-    ),
+    shared.PartitionParameters(
+        files=files,
+        split_pdf_page=True,
+        split_pdf_allow_failed=True,
+        split_pdf_concurrency_level=15
+    )
 )
 
 try:
     res = client.general.partition(request=req)
-    print(res.elements[0])
+    element_dicts = [element for element in res.elements]
+    json_elements = json.dumps(element_dicts, indent=2)
+
+    # Print the processed data.
+    print(json_elements)
+
+    # Write the processed data to a local file.
+    with open(output_filepath, "w") as file:
+        file.write(json_elements)
 except Exception as e:
     print(e)
