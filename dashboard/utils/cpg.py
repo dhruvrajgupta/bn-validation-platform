f609d5159f291c9da4f0c2276b6618af2a61ab73
dhruv.raj <dhruv.gupta@the-chainless.com>
2024-09-18 13:53:11 +0200

    util function for prompting llm

--- dashboard/utils/cpg.py
+++ dashboard/utils/cpg.py
@@ -1,4 +1,5 @@
 import re
+from openai import OpenAI
 
 def split_sequence(sequence, chunk_size):
     """
@@ -63,3 +64,57 @@ def format_annotated_text(text):
                 result.append(f" {content} ")
 
     return result
+
+
+def ask_llm(prompt: str, stream=False):
+    """
+    Ask the LLM a question and print the response.
+
+    Parameters
+    ----------
+    prompt : str
+        The question to ask the LLM.
+    stream : bool, optional
+        If True, stream the response from the LLM as it is generated. If False, wait for the
+        entire response to be generated before printing it. Defaults to False.
+
+    Returns
+    -------
+    str
+        The response from the LLM.
+    """
+
+    model_name = "gpt-4o-mini"
+    temperature = 0
+
+    client = OpenAI(
+        # Defaults to os.environ.get("OPENAI_API_KEY")
+    )
+
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+        temperature=temperature,
+        stream=stream
+    )
+
+    print(f"START OF LLM RESPONSE: \n{'-'*20}\n")
+
+    if stream:
+        llm_response = ""
+
+        for chunk in response:
+            if chunk.choices[0].delta.content:
+                llm_response += chunk.choices[0].delta.content
+                print(chunk.choices[0].delta.content, end='', flush=True)
+
+    else:
+        llm_response = response.choices[0].message.content
+        print(llm_response)
+
+    print(f"\n\nEND OF LLM RESPONSE\n{'-'*50}\n")
+
+    return llm_response
