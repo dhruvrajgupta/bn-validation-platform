f4d380b07d48572f15bbcedc1dfa4ade42c9111a
dhruv.raj <dhruv.gupta@the-chainless.com>
2024-09-21 14:02:18 +0200

    Test DB connection

--- dashboard/pages/length_calc.py
+++ dashboard/pages/length_calc.py
@@ -3,3 +3,28 @@ import streamlit as st
 st.sidebar.markdown("Length calculator")
 
 st.markdown("Length calculator")
+
+import pymongo
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return pymongo.MongoClient(**st.secrets["mongo"])
+
+client = init_connection()
+
+# Pull data from the collection.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def get_data():
+    db = client["bn-validation"]
+    items = db.pages.find()
+    items = list(items)  # make hashable for st.cache_data
+    return items
+
+items = get_data()
+
+# Print results.
+for item in items:
+    st.write(f"{item['name']} has a :{item['pet']}:")
