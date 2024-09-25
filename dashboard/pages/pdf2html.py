6d449fc71aab694de0d22c2c3a08f8872c90c210
dhruv.raj <dhruv.gupta@the-chainless.com>
2024-09-25 14:54:53 +0200

    Select Guideline and its pages view

--- dashboard/pages/pdf2html.py
+++ dashboard/pages/pdf2html.py
@@ -1,11 +1,69 @@
 import streamlit as st
+import streamlit_antd_components as sac
+
+guideline_map = {
+    "NCCN": {
+        "name":  "NCCN Guidelines Head and Neck Cancers v4.2024",
+        "no_of_pages": 6,
+        "dir_name": "NCCN_TNMLC",
+    },
+    "S3": {
+        "name": "S3 guideline - Diagnosis, therapy and aftercare of laryngeal carcinoma",
+        "no_of_pages": 147,
+        "dir_name": "S3LC",
+    }
+}
+
+def code_to_name(guideline_code):
+    return guideline_map[guideline_code]["name"]
 
 st.set_page_config(layout="wide")
 
-st.sidebar.markdown("Temperature calculator")
+with st.sidebar:
+    selected_guideline = st.selectbox(
+        "Select Guideline",
+        ("NCCN", "S3"),
+        format_func=lambda x: guideline_map[x]["name"],
+        index=0,
+        placeholder="Select guideline...",
+    )
+
+    #### EXAMPLE OF WELL FORMATED TABLE OF CONTENTS ####
+    # selected_page = sac.tree(items=[
+    #     sac.TreeItem('TNM Staging System for the Larynx, ST-8', children=[
+    #         sac.TreeItem('Primary Tumor(T), ST-8', children=[
+    #             sac.TreeItem('Supraglottis, ST-8'),
+    #             sac.TreeItem('Glottis, ST-8'),
+    #             sac.TreeItem('Subglottis, ST-8')
+    #         ]),
+    #         sac.TreeItem('Regional Lymph Nodes(N), ST-9', children=[
+    #             sac.TreeItem('Clinical N (cN), ST-9'),
+    #             sac.TreeItem('Pathalogical N (pN), ST-10'),
+    #         ]),
+    #         sac.TreeItem('Distant Metastasis(M), ST-10'),
+    #         sac.TreeItem('Histologic Grade(G), ST-10'),
+    #         sac.TreeItem('Prognostic Stage Groups, ST-10'),
+    #         sac.TreeItem('Discussion, MS-44', children=[
+    #             sac.TreeItem('Cancer of the Larynx, MS-44', children=[
+    #                 sac.TreeItem('Workup and Staging, MS-44'),
+    #                 sac.TreeItem('Treatment, MS-44'),
+    #                 sac.TreeItem('Treatment (contd.), MS-45'),
+    #                 sac.TreeItem('Radiation Therapy Fractionation, MS-46'),
+    #                 sac.TreeItem('Follow-up/Surveillance, MS-46')
+    #             ])
+    #         ])
+    #     ])
+    # ], label='NCCN Guidelines Head and Neck Cancers v4.2024', index=0, checkbox_strict=False, open_all=True)
+
+    items = [sac.TreeItem(f"Page - {i+1}") for i in range(guideline_map[selected_guideline]['no_of_pages'])]
+    selected_page = sac.tree(items=items, label=f"**{guideline_map[selected_guideline]['name']}**", index=0, checkbox_strict=False, open_all=True)
+    selected_page_no = selected_page.split("-")[-1].strip()
 
 if not "page_sections_info" in st.session_state:
     st.session_state.page_sections_info = {}
+
+if not "data_source" in st.session_state:
+    st.session_state.data_source = None
 
 @st.dialog("View Prompt", width="large")
 def show_prompt():
@@ -25,7 +83,7 @@ guideline_page, data_extractions = st.tabs(["Guideline Page", "Data Extractions"
 
 with guideline_page:
     with st.container(border=True):
-        st.markdown("<iframe src='http://localhost:3000/NCCN_TNMLC/1.html' style='width:calc(100%);height:960px; overflow:auto'></iframe>", unsafe_allow_html=True)
+        st.markdown(f"<iframe src='http://localhost:3000/{guideline_map[selected_guideline]['dir_name']}/{selected_page_no}.html' style='width:1280px;height:960px; overflow:auto'></iframe>", unsafe_allow_html=True)
 
 with data_extractions:
     html_page, data_extractions = st.columns(2)
@@ -45,7 +103,7 @@ with data_extractions:
             source_code = HtmlFile.read()
             # st.components.v1.html("<iframe src='http://localhost:3000/NCCN_TNMLC/1.html' style='width:calc(100%);height:calc(100%); overflow:auto'></iframe>", height=960)
             # st.components.v1.iframe("http://localhost:3000/NCCN_TNMLC/1.html", height = 920, width=calc(100%), scrolling=True)
-            st.markdown("<iframe src='http://localhost:3000/NCCN_TNMLC/1.html' style='width:calc(100%);height:960px; overflow:auto'></iframe>", unsafe_allow_html=True)
+            st.markdown(f"<iframe src='http://localhost:3000/{guideline_map[selected_guideline]['dir_name']}/{selected_page_no}.html' style='width:calc(100%);height:960px; overflow:auto'></iframe>", unsafe_allow_html=True)
     with data_extractions:
         with st.container(border=True):
             # st.markdown(f"**Data Extractions:**")
@@ -78,15 +136,18 @@ with data_extractions:
                 with st.spinner(f"Extracting data...{sections}"):
                     prompt = DATA_EXTRACTOR.format(html_page=source_code, sections=sections)
                     extracted_data = json.loads(ask_llm_response_schema(prompt, response_format=ListSectionData))["result"]
+                    st.session_state.page_sections_info = {}
                     for section in extracted_data:
                         paragraph_content = ""
                         for idx, para in enumerate(section['paragraph']):
                             paragraph_content += f"{idx+1}. {para}\n"
                         st.session_state.page_sections_info[section["section_name"]] = paragraph_content
+                        st.session_state.data_source = "GPT-4o-mini"
 
             if st.session_state.page_sections_info:
                 selected_section = st.radio("Select Section:", list(st.session_state.page_sections_info.keys()), label_visibility="collapsed", horizontal=True)
                 display_section(selected_section)
+                st.markdown(f"**Data Source:** {st.session_state.data_source}")
 
 with st.expander("Session State"):
     st.write(st.session_state)
