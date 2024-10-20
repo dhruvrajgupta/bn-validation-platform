82967ba4a5764b7f220c3bde13556a1224ae5f9b
dhruv.raj <dhruv.gupta@the-chainless.com>
2024-10-20 19:00:01 +0200

    Fix Display New Models

--- dashboard/entrypoint.py
+++ dashboard/entrypoint.py
@@ -7,17 +7,15 @@ instructions_manual = st.Page("page_views/👋_Instructions_Manual.py")
 
 ## Models
 new_model = st.Page("page_views/models/new_model.py", title="New Model")
-label_descriptions_nodes_contents = st.Page("page_views/models/label_description_nodes_contents.py", title="Label and Descriptions")
+# label_descriptions_nodes_contents = st.Page("page_views/models/label_description_nodes_contents.py", title="Label and Descriptions")
 
 ## Nodes
-temp = st.Page("page_views/nodes/temp.py")
 
 
 pg = st.navigation(
     {
         "Instructions Manual": [instructions_manual],
-        "Models": [new_model, label_descriptions_nodes_contents],
-        "Nodes": [temp],
+        "Models": [new_model],
     }
 )
 
