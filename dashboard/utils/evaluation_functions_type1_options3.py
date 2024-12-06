6ce5d8f86733606ba1ecaa3e0af53f3b670d8532
dhruv.raj <dhruv.gupta@the-chainless.com>
2024-12-06 01:39:34 +0100

    3 options

---
+++ dashboard/utils/evaluation_functions_type1_options3.py
@@ -0,0 +1,373 @@
+from enum import Enum
+from pydantic import BaseModel
+from typing import List, Optional
+
+import json
+import openai
+import weave
+
+import asyncio
+import numpy as np
+from math import exp
+
+import streamlit as st
+
+############## PORTION WITHIN THIS SECTION IS COMMON FOR ALL EVALUATIONS ##############
+
+# Example
+# dataset = [
+#     {"id": 1, "edge": (), "correct": "B", "incorrect": "A", "prompt": "..."}
+# ]
+
+def format_reasoning(evaluation_data):
+    for id, evaluation_data_item in enumerate(evaluation_data):
+        # print(json.dumps(evaluation_data, indent=2))
+        list_reasoning = evaluation_data_item.get("reasoning")
+        # st.write(list_reasoning)
+        if list_reasoning:
+            formatted_reasoning = ""
+            for step, reason in enumerate(list_reasoning):
+                formatted_reasoning += f"{step + 1}. {reason}\n"
+            evaluation_data[id]["reasoning"] = formatted_reasoning
+
+        list_critique_reasoning = evaluation_data_item.get("critique_reasoning")
+        # print(list_critique_reasoning)
+        if list_critique_reasoning:
+            formatted_critique_reasoning = ""
+            for step, reason in enumerate(list_critique_reasoning):
+                formatted_critique_reasoning += f"{step + 1}. {reason}\n"
+            evaluation_data[id]["critique_reasoning"] = formatted_critique_reasoning
+
+    return evaluation_data
+
+CRITIQUE_PROMPT = """\
+Analyze the output from an AI assistant. 
+Is the final answer ({ini_answer}) consistent with the reasoning provided by the assistant?  
+
+Question: {prompt_from_before}  
+AI assistant: {answer_from_before}
+
+The answer is: ...
+
+DESIRED OUTPUT FORMAT:
+<critique_thinking>
+...
+</critique_thinking>
+<critique_answer>
+{{
+   "critique_thinking": ["...", ...]
+   "critique_answer": ...
+}}
+</critique_answer>
+
+Before providing the answer in <critique_answer> tags, think step by step in detail in <critique_thinking> tags and analyze every part.
+Output inside <critique_answer> tag in JSON format. Only output valid JSON.
+DO NOT HALLUCINATE. DO NOT MAKE UP FACTUAL INFORMATION.
+"""
+
+class Option(str, Enum):
+    A = "A"
+    B = "B"
+    C = "C"
+
+class EdgeOrientationJudgement(BaseModel):
+    # id: int
+    thinking: List[str]
+    answer: Option
+
+class CritiqueOption(str, Enum):
+    A = "A"
+    B = "B"
+    C = "C"
+
+class CritiqueResponse(BaseModel):
+    critique_thinking: List[str]
+    critique_answer: CritiqueOption
+
+class EvaluationModel(weave.Model):
+    model_name: str
+
+    @weave.op()
+    async def predict(self, prompt: str) -> dict:
+        client = openai.AsyncClient()
+
+        response = await client.beta.chat.completions.parse(
+            model=self.model_name,
+            messages=[
+                {"role": "user", "content": prompt}
+            ],
+            response_format=EdgeOrientationJudgement,
+            temperature=0,
+            logprobs=True,
+            top_logprobs=3,
+            seed=123
+        )
+        result = response.choices[0].message.content
+
+        answer_choice_probablities = {
+            "A": None,
+            "B": None,
+            "C": None
+        }
+
+        # import numpy as np
+        # from math import exp
+        for token in response.choices[0].logprobs.content[-2].top_logprobs:
+        #     print("Token:", token.token)
+        #     print("Log prob:", token.logprob)
+        #     print("Linear prob:", np.round(exp(token.logprob) * 100, 2), "%")
+        #     print("Bytes:", token.bytes, "\n")
+
+            if token.token == "A":
+                answer_choice_probablities["A"] = np.round(exp(token.logprob) * 100, 2)
+            elif token.token == "B":
+                answer_choice_probablities["B"] = np.round(exp(token.logprob) * 100, 2)
+            elif token.token == "C":
+                answer_choice_probablities["C"] = np.round(exp(token.logprob) * 100, 2)
+        # print(answer_choice_probablities)
+        #
+        # answer_choice_token_log_prob_content = response.choices[0].logprobs.content[-2]
+        # if answer_choice_token_log_prob_content.token == "B":
+        #     answer_choice_probablities["B"] = np.round(exp(answer_choice_token_log_prob_content.logprob) * 100, 2)
+        #     answer_choice_probablities["A"] = np.round(100.0 - answer_choice_probablities["B"], 2)
+        # else:
+        #     answer_choice_probablities["A"] = np.round(exp(answer_choice_token_log_prob_content.logprob) * 100, 2)
+        #     answer_choice_probablities["B"] = np.round(100.0 - answer_choice_probablities["A"],2)
+
+        if result is None:
+            raise ValueError("No response from model")
+        parsed = json.loads(result)
+        ini_parsed = json.loads(result)
+        parsed["answer_choice_probablities"] = answer_choice_probablities
+
+        #### Critique ####
+
+        # print(CRITIQUE_PROMPT.format(
+        #     prompt_from_before=prompt,
+        #     answer_from_before=ini_parsed,
+        #     ini_answer=ini_parsed["answer"]
+        # ))
+        critique_response = await client.beta.chat.completions.parse(
+            model=self.model_name,
+            messages=[
+                {"role": "user", "content": CRITIQUE_PROMPT.format(
+                    prompt_from_before=prompt,
+                    answer_from_before=ini_parsed,
+                    ini_answer=ini_parsed["answer"]
+                )}
+            ],
+            response_format=CritiqueResponse,
+            temperature=0,
+            logprobs=True,
+            # top_logprobs=3,
+            seed=123
+        )
+        critique_result = critique_response.choices[0].message.content
+        # print(critique_result)
+        if critique_result is None:
+            raise ValueError("No response from model")
+        critique_parsed = json.loads(critique_result)
+
+        parsed["critique"] = critique_parsed
+
+        return parsed
+
+#######################################################################################
+
+
+######## DIFFERENT EVALUATIONS START FROM HERE ########
+
+
+# ######## USING NODE IDENTIFIERS AND THEIR STATE NAMES | CAUSAL VERB: causes ########
+# NODE_ID_AND_STATE_NAMES = """\
+# NODE 1:
+# node_id: {n1}
+# states: {n1_states}
+#
+# NODE 2:
+# node_id: {n2}
+# states: {n2_states}
+#
+# Among these three options which one is the most likely true:
+#
+# (A) {n1} {verbk} {n2}
+# (B) {n2} {verbk} {n1}
+# (C) The is no direct causal relation between {n1} and {n2}
+#
+# The answer is: ...
+#
+# DESIRED OUTPUT FORMAT:
+# <thinking>
+# ...
+# </thinking>
+# <answer>
+# {{
+#    "thinking": ["...", ...]
+#    "answer": ...
+# }}
+# </answer>
+#
+# Before providing the answer in <answer> tags, think step by step in <thinking> tags and analyze every part.
+# Output inside <answer> tag in JSON format. Only output valid JSON.
+# DO NOT HALLUCINATE. DO NOT MAKE UP FACTUAL INFORMATION.
+# """
+#
+# def baseline_only_node_id_state_names_causes(incorrect_edges, eval_name, llm_model_name, bn_model):
+#     dataset = []
+#     evaluation_data = []
+#
+#     @weave.op()
+#     def edge_judgement_scorer(id, output):
+#         evaluation_data[id] = dataset[id]
+#         evaluation_data[id]["reasoning"] = output["thinking"]
+#         evaluation_data[id]["answer"] = output["answer"]
+#         evaluation_data[id]["answer_choice_probablities"] = output["answer_choice_probablities"]
+#         return output["answer"] == dataset[id]["correct"]
+#
+#     def create_dataset(incorrect_edges, prompt_template):
+#         # We are reversing the edges for evaluation
+#         for id, edge in enumerate(incorrect_edges):
+#             n1 = edge[0]
+#             n2 = edge[1]
+#             n1_states = bn_model.states[n1]
+#             n2_states = bn_model.states[n2]
+#             data = {
+#                 "id": id,
+#                 "llm_model_name": llm_model_name,
+#                 "edge": edge,
+#                 "n1_states": n1_states,
+#                 "n2_states": n2_states,
+#                 "verb": "causes",
+#                 "correct": "B",
+#                 # "incorrect": "A",
+#                 "prompt": prompt_template.format(
+#                     n1=n1, n2=n2,
+#                     n1_states=n1_states, n2_states=n2_states,
+#                     verbk="causes",
+#                     id=id)
+#             }
+#             dataset.append(data)
+#             evaluation_data.append(data)
+#
+#     create_dataset(incorrect_edges, NODE_ID_AND_STATE_NAMES)
+#
+#     weave.init('bnv_N_staging')
+#
+#     model = EvaluationModel(model_name=llm_model_name)
+#
+#     evaluation = weave.Evaluation(
+#         name=eval_name,
+#         dataset=dataset,
+#         scorers=[edge_judgement_scorer]
+#     )
+#
+#     evaluation_scorer_summary = asyncio.run(evaluation.evaluate(model))
+#
+#     eval_res_dict = {
+#         # "dataset": dataset,
+#         "eval_scorer_summary": evaluation_scorer_summary,
+#         "evaluation_data": format_reasoning(evaluation_data)
+#     }
+#
+#     return eval_res_dict
+
+
+
+#### EVALUATION WITH NODE ID AND DIFFERENT CAUSAL VERBS
+# (C) The is no direct relation between {n1} and {n2}
+ONLY_NODE_ID_DIFF_CAUSAL_VERB = """\
+Among these three options which one is the most likely true: 
+
+NODE1: {n1}
+NODE2: {n2}
+
+(A) {n1} {verbk} {n2} 
+(B) {n2} {verbk} {n1} 
+(C) There is no relation between `{n1}` and `{n2}`
+
+The answer is: ...
+
+DESIRED OUTPUT FORMAT:
+<thinking>
+...
+</thinking>
+<answer>
+{{
+   "thinking": ["...", ...]
+   "answer": ...
+}}
+</answer>
+
+Before providing the answer in <answer> tags, think step by step in detail in <thinking> tags and analyze every part.
+Output inside <answer> tag in JSON format. Only output valid JSON.
+DO NOT HALLUCINATE. DO NOT MAKE UP FACTUAL INFORMATION.
+"""
+
+def baseline_only_node_id_causal_verb(incorrect_edges, eval_name, llm_model_name, bn_model):
+    causal_verb = eval_name.split("_")[-1]
+
+    dataset = []
+    evaluation_data = []
+
+    @weave.op()
+    def edge_judgement_scorer(id, output):
+        # print(json.dumps(output, indent=4))
+        evaluation_data[id] = dataset[id]
+        evaluation_data[id]["reasoning"] = output["thinking"]
+        evaluation_data[id]["answer"] = output["answer"]
+        evaluation_data[id]["answer_choice_probablities"] = output["answer_choice_probablities"]
+        evaluation_data[id]["critique_consistent"] = (
+            "yes" if output["critique"]["critique_answer"] == output["answer"] else "no")
+        evaluation_data[id]["critique_answer"] = output["critique"]["critique_answer"]
+        evaluation_data[id]["critique_reasoning"] = output["critique"]["critique_thinking"]
+        return output["answer"] == dataset[id]["correct"]
+
+    @weave.op()
+    def edge_judgement_consistency_scorer(id, output):
+        return output["answer"] == output["critique"]["critique_answer"]
+
+    def create_dataset(incorrect_edges, prompt_template):
+        # We are reversing the edges for evaluation
+        for id, edge_item in enumerate(incorrect_edges):
+            schema_dep_valid = edge_item["schema_dep_validity"]
+            edge = edge_item["edge"]
+            n1 = edge[0]
+            n2 = edge[1]
+            data = {
+                "id": id,
+                "llm_model_name": llm_model_name,
+                "edge": edge,
+                "schema_dep_validity": schema_dep_valid,
+                "verb": causal_verb,
+                "num_options": 3,
+                "correct": "B",
+                # "incorrect": "A",
+                "prompt": prompt_template.format(
+                    n1=n1, n2=n2,
+                    verbk=causal_verb,
+                    id=id)
+            }
+            dataset.append(data)
+            evaluation_data.append(data)
+
+    create_dataset(incorrect_edges, ONLY_NODE_ID_DIFF_CAUSAL_VERB)
+
+    weave.init('bnv_N_staging')
+
+    model = EvaluationModel(model_name=llm_model_name)
+
+    evaluation = weave.Evaluation(
+        name=eval_name,
+        dataset=dataset,
+        scorers=[edge_judgement_scorer, edge_judgement_consistency_scorer]
+    )
+
+    evaluation_scorer_summary = asyncio.run(evaluation.evaluate(model))
+
+    eval_res_dict = {
+        # "dataset": dataset,
+        "eval_scorer_summary": evaluation_scorer_summary,
+        "evaluation_data": format_reasoning(evaluation_data)
+    }
+
+    return eval_res_dict
