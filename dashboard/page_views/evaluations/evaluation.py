a58ad7444e6fd10f1f58f19cc8a82fd93264befe
dhruv.raj <dhruv.gupta@the-chainless.com>
2024-12-08 22:14:27 +0100

    Type 2 Augmentations with States descriptions

--- dashboard/page_views/evaluations/evaluation.py
+++ dashboard/page_views/evaluations/evaluation.py
@@ -316,3 +316,12 @@ else:
                 from utils.evaluation_functions_type2_options2 import node_id_state_names_node_type_observability_label_description
                 evaluation_function = node_id_state_names_node_type_observability_label_description
                 trigger_evaluation(evaluation_function, evaluation_name)
+
+            evaluation_name = f"type2_node_id__node_type_observability_labels_description_states_descriptions"
+            if st.checkbox(
+                    f"**[Augmentation] Node Type, Observability, Node Label, Description and States Descriptions**",
+                    key=evaluation_name):
+                from utils.evaluation_functions_type2_options2 import \
+                    node_id_node_type_observability_label_description_states_descriptions
+                evaluation_function = node_id_node_type_observability_label_description_states_descriptions
+                trigger_evaluation(evaluation_function, evaluation_name)
