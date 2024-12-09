8189f0b5258054f680ccb2236b1742911bf31551
dhruv.raj <dhruv.gupta@the-chainless.com>
2024-12-09 22:10:32 +0100

    Type 2 Causal Factors

--- dashboard/page_views/evaluations/evaluation.py
+++ dashboard/page_views/evaluations/evaluation.py
@@ -331,3 +331,12 @@ else:
                     node_id_node_type_observability_label_description_states_descriptions
                 evaluation_function = node_id_node_type_observability_label_description_states_descriptions
                 trigger_evaluation(evaluation_function, evaluation_name)
+
+            evaluation_name = f"type2_node_id__node_type_observability_labels_description_states_descriptions_causal_factors"
+            if st.checkbox(
+                    f"**[Augmentation] Node Type, Observability, Node Label, Description, States Descriptions and Causal Factors**",
+                    key=evaluation_name):
+                from utils.evaluation_functions_type2_options2 import \
+                    node_id_node_type_observability_label_description_states_descriptions_causal_factors
+                evaluation_function = node_id_node_type_observability_label_description_states_descriptions_causal_factors
+                trigger_evaluation(evaluation_function, evaluation_name)
